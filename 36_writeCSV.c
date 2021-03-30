9c460697defca556980144c93012f6f1b7f13fb5
sudhanshu bhagat <2020a1r158@mietjammu.in>
2021-03-30 23:45:22 +0530

    Write .csv file

---
+++ 36_writeCSV.c
@@ -0,0 +1,42 @@
+//PROGRAM TO WRITE .csv FILES.
+// Written by -- SUDHANSHU BHAGAT
+//30-03-2021
+
+#include <stdio.h> //Pre-Process Directive to Include standard input output header files.
+#include <stdlib.h>//Pre-Process Directive to Include standard library header files.
+#include <string.h>//re-Process Directive to Include string header files. 
+
+int main() //Main function body starting
+{ 
+    FILE* fp = fopen("C:\\Users\\hp\\Desktop\\C_programs\\Sudhanshu_CPrograms0\\36_WriteCSV.csv", "a+"); //File path.
+
+    char name[50]; 
+    int rollno, marks; 
+
+    if (!fp) { 
+        // Error in file opening 
+        printf("Can't open file\n"); 
+        return 0; 
+    }
+
+    //Taking input from the user for the new record to be added.   
+    printf("\nEnter Student Name\n"); 
+    scanf("%s", &name); 
+
+    printf("\nEnter Roll Number\n"); 
+    scanf("%d", &rollno); 
+
+    printf("\nEnter Attendance\n"); 
+    scanf("%d", &marks); 
+
+
+    // Saving data in file 
+    fprintf(fp, "%s, %d, %d\n", name, 
+            rollno, marks); 
+
+    printf("\nNew student deatils added."); 
+
+    fclose(fp); 
+    return 0; 
+
+} //main function body ends
