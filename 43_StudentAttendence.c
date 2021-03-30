f55961c8b7097299d66d85cd9fdc54a4edd62257
sudhanshu bhagat <2020a1r158@mietjammu.in>
2021-03-31 00:16:45 +0530

    Students Attendence detail

---
+++ 43_StudentAttendence.c
@@ -0,0 +1,35 @@
+// It reads file prepared in the previous program and then interacts with user asking any student's roll number and prints his/her attendance details. *
+//Writted by -- SUDHANSHU BHAGAT
+//30-03-2021
+
+#include <stdio.h> //Pre process directive to include standard input output functions header file
+int main() { //main function body
+
+    printf("The mimimum attendance for giving the examination is 75 percent. \n");
+    printf("enter details to check eligibility:\n");
+    float a, b;
+    printf("enter the total no. of classes held.\n");
+    scanf("%f", &a);
+    printf("enter the no. of classes attended.\n");
+    scanf("%f", &b);
+
+    if((b/a) * 100 < 75) {
+        printf("your attendance is less than 75%.\n");
+        char c;
+        printf("Do you have a medical cause? 'Y' for yes and 'N' for no.\n");
+        scanf(" %ch", &c);
+    switch(c) {
+    case 'Y':
+        printf("you are eligible for giving the examinations.");
+          break;
+      case 'N': 
+      printf("sorry, you are not eligible for giving the examinations.");
+      break;
+      default:
+          printf("Invalid input.");
+  }
+    } else {
+        printf("you are eligible for giving the examinations.");
+    }
+    return 0;
+}
