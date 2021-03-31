2fe3891708fcb0d5af50ec95c6acf8218f4120e1
sudhanshu bhagat <2020a1r158@mietjammu.in>
2021-04-01 00:39:42 +0530

    Take 10 values from the user and store them in a array

--- 37_ArrayDeclaration.c
+++ 37_ArrayDeclaration.c
@@ -1,23 +1,23 @@
-// Program to take 5 values from the user and store them in an array
+// Program to take 10 values from the user and store them in an array
 //Writted by -- SUDHANSHU BHAGAT
 //30-03-2021
 
 #include <stdio.h> //Pre-Process directive to include standard input output header file.
 
 int main() { // main function body starting
-  int values[5]; // Array Declaration of size 5
+  int values[10]; // Array Declaration of size 10
 
-  printf("Enter 5 integers: "); //input integers
+  printf("Enter 10 integers: "); //input integers
 
   // taking input and storing it in an array
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 10; ++i) {
      scanf("%d", &values[i]);
   }
 
   printf("Displaying integers: ");
 
   // printing elements of an array
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 10; ++i) {
      printf("%d\n", values[i]);
   }
   return 0;
